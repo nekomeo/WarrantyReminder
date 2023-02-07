5cad2702a14f23a63f3af1009ba7922c747f9820
Linh Tu <nekomeo@users.noreply.github.com>
2023-02-07 13:06:32 -0800

    Project re-creation using Storyboard instead

---
+++ ios/WarrantyReminder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WarrantyReminder
+//
+//  Created by Elle Tee on 2023-02-07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
