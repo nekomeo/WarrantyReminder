3f29c0364154ca9baac98461ee0834711057bfdb
Linh Tu <nekomeo@users.noreply.github.com>
2023-02-07 13:22:39 -0800

    Hardcode list of 10 rows

--- ios/WarrantyReminder/ViewController.swift
+++ ios/WarrantyReminder/ViewController.swift
@@ -7,13 +7,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+  let tableView = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+      view.addSubview(tableView)
 
+      tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+      tableView.delegate = self
+      tableView.dataSource = self
+    }
+  
+  override func viewDidLayoutSubviews() {
+    super.viewDidLayoutSubviews()
+    tableView.frame = view.bounds
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 10
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+    
+    cell.textLabel?.text = "Cell \(indexPath.row + 1)"
+    return cell
+  }
+  
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    tableView.deselectRow(at: indexPath, animated: true)
+    print("cell tapped")
+  }
 
 }
 
