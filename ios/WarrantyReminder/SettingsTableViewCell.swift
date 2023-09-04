39a9c5a637242289bc57cf6cee20a724881eb0c6
Linh Tu <nekomeo@users.noreply.github.com>
2023-09-04 16:50:43 -0700

    Add SettingsTableViewCell to project

---
+++ ios/WarrantyReminder/SettingsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SettingsTableViewCell.swift
+//  WarrantyReminder
+//
+//  Created by Elle Tee on 2023-09-04.
+//
+
+import UIKit
+
+class SettingsTableViewCell: UITableViewCell {
+	static let identifier = "SettingsTableViewCell"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
