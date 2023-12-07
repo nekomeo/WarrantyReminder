45bab4e1956aed0a73e0c83eed0d8d8dc977d69a
Linh Tu <nekomeo@users.noreply.github.com>
2023-12-07 14:38:30 -0800

    Refactor CalendarVC setup

--- ios/WarrantyReminder/CalendarViewController.swift
+++ ios/WarrantyReminder/CalendarViewController.swift
@@ -9,39 +9,45 @@ import UIKit
 import FSCalendar
 
 class CalendarViewController: UIViewController, FSCalendarDelegate {
+	private let segmentItems = ["All", "Paid", "Unpaid"]
 	
-	var calendar = FSCalendar()
-	
-	let segmentItems = ["All", "Paid", "Unpaid"]
-	let calendarContainerView = UIView(frame: CGRect(x: 0, y: 100, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.width))
-	let calendarTableContainerView = UIView(frame: CGRect(x: 0, y: 500, width: UIScreen.main.bounds.width, height: 100))
-	
-	let calendarTableView = UITableView(frame: CGRect(x: 0, y: 600, width: 40, height: 40))
+	private let calendarContainerView: UIView = {
+		let calendarContainerView = UIView()
+		calendarContainerView.translatesAutoresizingMaskIntoConstraints = false
+		
+		return calendarContainerView
+	}()
+	private let calendar: FSCalendar = {
+		let calendar = FSCalendar()
+		calendar.translatesAutoresizingMaskIntoConstraints = false
+		calendar.backgroundColor = .systemGray4
+		
+		return calendar
+	}()
+	private let calendarTableView: UITableView = {
+		let calendarTableView = UITableView()
+		calendarTableView.translatesAutoresizingMaskIntoConstraints = false
+		
+		return calendarTableView
+	}()
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
-		self.view.backgroundColor = UIColor.systemPurple
+		self.view.backgroundColor = .systemPurple
+		self.title = "Calendar"
 		
-		calendarTableContainerView.addSubview(calendarTableView)
+		calendarContainerView.addSubview(calendar)
+		calendar.delegate = self
+		
+		calendarContainerView.addSubview(calendarTableView)
 		calendarTableView.register(CalendarTableViewCell.self, forCellReuseIdentifier: CalendarTableViewCell.identifier)
 		calendarTableView.delegate = self
 		calendarTableView.dataSource = self
 		
-//		tableView.translatesAutoresizingMaskIntoConstraints = false
-//		tableView.topAnchor.constraint(equalTo: calendarView.bottomAnchor, constant: 0).isActive = true
-//		tableView.bottomAnchor.constraint(equalTo: view.topAnchor, constant: -100).isActive = true
-//		tableView.leadingAnchor.constraint(equalTo: calendarView.leadingAnchor, constant: 0).isActive = true
-//		tableView.trailingAnchor.constraint(equalTo: calendarView.trailingAnchor, constant: 0).isActive = true
-		
-		calendarContainerView.backgroundColor = .white
-		calendarTableContainerView.backgroundColor = .systemCyan
-		self.title = "Calendar"
-		
 		view.addSubview(calendarContainerView)
-		view.addSubview(calendarTableContainerView)
-
-		calendar.delegate = self
+		
+		layoutContraints()
 	}
 	
 	override func viewDidLayoutSubviews() {
@@ -54,45 +60,51 @@ class CalendarViewController: UIViewController, FSCalendarDelegate {
 		filterSegment.selectedSegmentTintColor = UIColor.systemBlue
 		filterSegment.setTitleTextAttributes([.foregroundColor: UIColor.systemBlue], for: .normal)
 		filterSegment.setTitleTextAttributes([.foregroundColor: UIColor.white], for: .selected)
-
-		calendar.frame = CGRect(x: 0, y: 100, width: view.frame.size.width, height: view.frame.size.width)
-		calendarTableView.frame = calendarTableContainerView.bounds
 		
 		self.navigationItem.titleView = filterSegment
 		self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(didPressAdd))
-		view.addSubview(calendar)
+	}
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+	}
+	
+	private func layoutContraints() {
+		NSLayoutConstraint.activate([
+			calendarContainerView.topAnchor.constraint(equalTo: self.view.layoutMarginsGuide.topAnchor, constant: 0.0),
+			calendarContainerView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 0.0),
+			calendarContainerView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: 0.0),
+			calendarContainerView.bottomAnchor.constraint(equalTo: self.view.layoutMarginsGuide.bottomAnchor, constant: 0.0)])
+		
+		NSLayoutConstraint.activate([
+			calendar.topAnchor.constraint(equalTo: calendarContainerView.topAnchor, constant: 0.0),
+			calendar.leadingAnchor.constraint(equalTo: calendarContainerView.leadingAnchor, constant: 0.0),
+			calendar.trailingAnchor.constraint(equalTo: calendarContainerView.trailingAnchor, constant: 0.0),
+			calendar.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.width)])
+		
+		NSLayoutConstraint.activate([
+			calendarTableView.topAnchor.constraint(equalTo: calendar.bottomAnchor, constant: 4.0),
+			calendarTableView.leadingAnchor.constraint(equalTo: calendarContainerView.leadingAnchor, constant: 0.0),
+			calendarTableView.trailingAnchor.constraint(equalTo: calendarContainerView.trailingAnchor, constant: 0.0),
+			calendarTableView.heightAnchor.constraint(equalToConstant: 200.0)])
 	}
 	
 	@objc func didPressAdd() {
 		let addBillVC = AddBillViewController()
 		let navController = UINavigationController(rootViewController: addBillVC)
-
+		
 		navController.modalPresentationStyle = .fullScreen
 		present(navController, animated: true)
-	}
-
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
 	}
 	
 	func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
 		let formatter = DateFormatter()
-
+		
 		formatter.dateFormat = "EEEE MM-dd-YYYY"
 		let dateString = formatter.string(from: date)
 		
 		print("\(dateString)")
 	}
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 }
 
 extension CalendarViewController: UITableViewDataSource, UITableViewDelegate {
@@ -104,8 +116,6 @@ extension CalendarViewController: UITableViewDataSource, UITableViewDelegate {
 		guard let cell = tableView.dequeueReusableCell(withIdentifier: CalendarTableViewCell.identifier, for: indexPath) as? CalendarTableViewCell else {
 			return UITableViewCell()
 		}
-		
-//		cell.configure(text: "Custom \(indexPath.row+1)")
 		
 		return cell
 	}
