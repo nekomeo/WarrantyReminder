adfb18af16ee5fd70da248f875d92ea0c972e9ac
Linh Tu <nekomeo@users.noreply.github.com>
2023-12-06 16:21:30 -0800

    Create UI container for AddBillVC

---
+++ ios/WarrantyReminder/AddBillTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AddBillTableViewCell.swift
+//  WarrantyReminder
+//
+//  Created by Elle Tee on 2023-12-06.
+//
+
+import UIKit
+
+class AddBillTableViewCell: UITableViewCell {
+	static let identifier = "AddBillTableViewCell"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- ios/WarrantyReminder/AddBillViewController.swift
+++ ios/WarrantyReminder/AddBillViewController.swift
@@ -8,6 +8,18 @@
 import UIKit
 
 class AddBillViewController: UIViewController {
+	private let addBillContainerView: UIView = {
+		let view = UIView()
+		view.backgroundColor = .systemMint
+		
+		return view
+	}()
+	
+	private let addBillTableView: UITableView = {
+		let view = UITableView()
+		
+		return view
+	}()
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -16,6 +28,28 @@ class AddBillViewController: UIViewController {
 		self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(didPressCancel))
 		self.navigationItem.title = "Add New Bill"
 		self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(didPressSave))
+		
+		self.view.addSubview(addBillContainerView)
+		addBillContainerView.addSubview(addBillTableView)
+		
+		addBillContainerView.translatesAutoresizingMaskIntoConstraints = false
+		addBillTableView.translatesAutoresizingMaskIntoConstraints = false
+		
+		addBillTableView.register(AddBillTableViewCell.self, forCellReuseIdentifier: AddBillTableViewCell.identifier)
+		addBillTableView.delegate = self
+		addBillTableView.dataSource = self
+		
+		NSLayoutConstraint.activate([
+			addBillContainerView.topAnchor.constraint(equalTo: self.view.layoutMarginsGuide.topAnchor, constant: 0.0),
+			addBillContainerView.bottomAnchor.constraint(equalTo: self.view.layoutMarginsGuide.bottomAnchor, constant: 0.0),
+			addBillContainerView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 0.0),
+			addBillContainerView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: 0.0)])
+		
+		NSLayoutConstraint.activate([
+			addBillTableView.topAnchor.constraint(equalTo: addBillContainerView.layoutMarginsGuide.topAnchor, constant: 0.0),
+			addBillTableView.bottomAnchor.constraint(equalTo: addBillContainerView.layoutMarginsGuide.bottomAnchor, constant: 0.0),
+			addBillTableView.leadingAnchor.constraint(equalTo: addBillContainerView.layoutMarginsGuide.leadingAnchor, constant: 0.0),
+			addBillTableView.trailingAnchor.constraint(equalTo: addBillContainerView.layoutMarginsGuide.trailingAnchor, constant: 0.0)])
 	}
 	
 	
@@ -37,4 +71,18 @@ class AddBillViewController: UIViewController {
 		self.dismiss(animated: true)
 	}
 	
+}
+
+extension AddBillViewController: UITableViewDataSource, UITableViewDelegate {
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		1
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		guard let cell = tableView.dequeueReusableCell(withIdentifier: AddBillTableViewCell.identifier, for: indexPath) as? AddBillTableViewCell else {
+			return UITableViewCell()
+		}
+		
+		return cell
+	}
 }
