65a55dfe7d31928eb27303b0d70ef178edc8a928
Linh Tu <nekomeo@users.noreply.github.com>
2023-12-28 13:26:44 -0800

    Add UISwitch to 'Auto Pay Bill' cell

--- ios/WarrantyReminder/AddBillTableViewCell.swift
+++ ios/WarrantyReminder/AddBillTableViewCell.swift
@@ -21,4 +21,45 @@ class AddBillTableViewCell: UITableViewCell {
 		// Configure the view for the selected state
 	}
 	
+	private let autoPaySwitch: UISwitch = {
+		let autoPaySwitch = UISwitch()
+		autoPaySwitch.translatesAutoresizingMaskIntoConstraints = false
+		autoPaySwitch.isOn = false
+		autoPaySwitch.addTarget(identifier.self, action: #selector(switchValueChanged(_:)), for: .valueChanged)
+		
+		return autoPaySwitch
+	}()
+	
+	override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+		super.init(style: style, reuseIdentifier: reuseIdentifier)
+	}
+	
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	func configure(with data: String) {
+		textLabel?.text = data
+		
+		// Remove the switchControl if it's already added
+		autoPaySwitch.removeFromSuperview()
+		
+		if data == "Auto Pay Bill" {
+			contentView.addSubview(autoPaySwitch)
+			
+			NSLayoutConstraint.activate([
+				autoPaySwitch.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+				autoPaySwitch.centerXAnchor.constraint(equalTo: contentView.centerXAnchor)
+			])
+		}
+	}
+	
+	@objc private func switchValueChanged(_ sender: UISwitch) {
+		if sender.isOn {
+			print("Switch is ON")
+		} else {
+			print("Switch is OFF")
+		}
+	}
+	
 }
